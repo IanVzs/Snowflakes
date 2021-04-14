3c1edd7514f47eb70d93ab109b96c47c1bda4ff8
Ian <ianvzs@outlook.com>
2021-04-14 16:26:01 +0800

    mv gin log 2 zap

--- routers/router.go
+++ routers/router.go
@@ -1,9 +1,12 @@
 package routers
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 
 	_ "github.com/IanVzs/Snowflakes/docs"
+	"github.com/IanVzs/Snowflakes/pkg/logging"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 
@@ -13,8 +16,9 @@ import (
 // InitRouter initialize routing information
 func InitRouter() *gin.Engine {
 	r := gin.New()
-	r.Use(gin.Logger())
-	r.Use(gin.Recovery())
+	// r.Use(gin.Logger())
+	// r.Use(gin.Recovery())
+	r.Use(logging.Ginzap(logging.Logger, time.RFC3339, true), logging.RecoveryWithZap(logging.Logger, true))
 	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	// test ws can use
 	/*curl --include --no-buffer --header "Connection: Upgrade" --header "Upgrade: websocket" --header "Host: 127.0.0.1:8000"  --header "Sec-WebSocket-Key: zVM4LLeZBgoAzNyTtkEjxGVbUEk="  --header "Sec-WebSocket-Version: 13" http://127.0.0.1:8000/ws*/
